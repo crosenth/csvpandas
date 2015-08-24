66bad14ac5c74cdbd0481aec86fd5622c4426489
Chris Rosenthal <crosenth@uw.edu>
2015-08-24 09:54:27 -0700

    added a few comments to the versioning function

--- csvpandas/utils.py
+++ csvpandas/utils.py
@@ -66,8 +66,9 @@ def read_csv(filename, compression=None, **kwargs):
 def version():
     """
     Depending on if git exists and at what version we can try these commands
-    to get the git version from the git tags.  If all else return the install
-    version or 0.0.0
+    to get the git version from the git tags.  Second, if the package has been
+    installed return the installed version.  Finally, if nothing else,
+    return 0.0.0
     """
 
     install_dir = os.path.dirname(__file__)
@@ -75,13 +76,17 @@ def version():
                 ['git', 'describe', '--tags'])  # < 1.8.5
     devnull = open(os.devnull, 'w')
 
+    """
+    try the two git commands above ^
+    git versions are organized as
+    [tag]-[number of commits over tag]-[commit id]
+    ex - v0.1.2-38-g6a8e5e3
+    """
     for cmd in git_cmds:
         try:
             logging.debug(' '.join(cmd))
+            git_re = '(?P<tag>[\d.]*)-?(?P<commit>[\d.]*)-?.*'
             git_ver = subprocess.check_output(cmd, stderr=devnull)
-            tag_re = '(?P<tag>[\d.]*)'
-            commit_re = '(?P<commit>[\d.]*)'
-            git_re = 'v{}-?{}-?.*'.format(tag_re, commit_re)
             git_search = re.search(git_re, git_ver)
             if git_search.group('commit') == '':
                 return git_search.group('tag')
