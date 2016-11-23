30acb37177add072948a4ca80a25a05940d425ae
Chris Rosenthal <crosenth@uw.edu>
2016-11-23 15:04:10 -0800

    install xlrd for reading excel files

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(author='Chris Rosenthal',
       entry_points={'console_scripts': {'csvpandas = csvpandas:main'}},
       version=csvpandas.version.version(),
       url='https://github.com/crosenth/csvpandas',
-      install_requires=['pandas>=0.17.1'],
+      install_requires=['pandas>=0.17.1', 'xlrd>=0.9.0'],
       license='GPLv3',
       classifiers=[
           'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
